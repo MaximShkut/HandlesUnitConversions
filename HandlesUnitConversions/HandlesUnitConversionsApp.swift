621258274603a91022104079abb12555cf630ec6
MaximShkut <m.shkut344@gmail.com>
2023-04-05 17:18:39 +0200

    Initial Commit

---
+++ HandlesUnitConversions/HandlesUnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandlesUnitConversionsApp.swift
+//  HandlesUnitConversions
+//
+//  Created by user236450 on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HandlesUnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
